9442147b6645846dc601698690b17611302858b9
Jenifer Ventura <84549239+Jenifer5307@users.noreply.github.com>
2021-12-10 01:10:53 -0300

    Create respondendo-ao-usuario.py

---
+++ ex003/respondendo-ao-usuario.py
@@ -0,0 +1,9 @@
+Nome=input("Nome:")
+Dia=input("Dia:")
+Mês=input("Mês:")
+Ano=input("Ano:")
+
+
+Print("Olá, ",Nome," você nasceu no dia " ,Dia, "do mês" ,Mês, "do ano" ,Ano,)
+z=input("Está correto?")
+Print("Obrigada, pelo feedback!" ,nome)
